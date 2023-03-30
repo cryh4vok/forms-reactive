27b56aabbbd99c94a3fffe9ef4c2cf97e2e71e17
Simon Pletersek <pletersek.si@gmail.com>
2023-03-30 10:55:44 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
   forbiddenUsernames = ['Chris', 'Anna'];
+  status = '';
 
   //! Building a form programmatically
   ngOnInit() {
@@ -20,16 +22,43 @@ export class AppComponent implements OnInit {
           Validators.required,
           this.forbiddenNames.bind(this),
         ]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
       }),
       gender: new FormControl('male'),
       hobbies: new FormArray([]),
+    });
+
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
+
+    this.signupForm.statusChanges.subscribe((value) => {
+      console.log(value);
+    });
+
+    this.signupForm.setValue({
+      userData: {
+        username: 'Simon',
+        email: 'pletersek.si@gmail.com',
+      },
+      gender: 'male',
+      hobbies: [],
+    });
+    this.signupForm.patchValue({
+      userData: {
+        username: 'Anna',
+      },
     });
   }
 
   //! We don't need to access the form from HTML, because we have built it ourselves.
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   getControls() {
@@ -40,11 +69,25 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
-
+  //! Custom validator
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return { nameIsForbidden: true };
     }
     return null;
+  }
+
+  //! Custom ASYNC validator
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
